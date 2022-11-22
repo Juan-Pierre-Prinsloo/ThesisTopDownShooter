b317d3bb8fb03e452fe0d3acdc26b1a389c965e3
Juan-Pierre Prinsloo <576598@student.belgiumcampus.ac.za>
2022-11-22 19:36:12 +0200

    Add: Added a start menu with difficulty selection.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void Easy()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);//add scene params here
+    }
+
+    public void Normal()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);//add scene params here
+    }
+
+    public void Hard()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);//add scene params here
+    }
+}
